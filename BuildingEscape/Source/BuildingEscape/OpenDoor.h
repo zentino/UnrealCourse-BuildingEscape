5ddfd6b0df0b171d190f6bbb8cc325a3fe5f0057
Unknown <sn-z777@outlook.com>
2018-08-28 09:05:21 +0200

    Moving Objects With C++ (set door rotation value)

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -2,19 +2,19 @@
 
 #pragma once
 
-#include "CoreMinimal.h" // instead of Engine.h, CoreMinimal.h is used for compilation speed improvements
+#include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "PositionReport.generated.h"
+#include "OpenDoor.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UPositionReport : public UActorComponent
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UPositionReport();
+	UOpenDoor();
 
 protected:
 	// Called when the game starts
